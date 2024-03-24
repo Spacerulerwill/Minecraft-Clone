5b0acb1df49986192755b0cfdc0ad7a8cf9dc6b1
William Redding <williamdredding@gmail.com>
2024-03-24 13:57:25 -0700

    Remove submodules from .tokeignore

--- src/world/World.hpp
+++ src/world/World.hpp
@@ -61,7 +61,7 @@ public:
     static constexpr int MAX_GEN_HEIGHT = (ChunkStack::DEFAULT_SIZE * Chunk::SIZE) - 1;
     static constexpr int MIN_GEN_HEIGHT = Chunk::SIZE - 1;
     static constexpr int MAX_SUB_MIN_GEN_HEIGHT = MAX_GEN_HEIGHT - MIN_GEN_HEIGHT;
-    static constexpr int WATER_LEVEL = MIN_GEN_HEIGHT + (Chunk::SIZE * 1);    
+    static constexpr int WATER_LEVEL = MIN_GEN_HEIGHT + (Chunk::SIZE * 4);    
     static_assert(WATER_LEVEL < MAX_GEN_HEIGHT);
     static constexpr double DAY_DURATION = 120.0;
     Vec3 mWaterColor = Vec3{ 68.0f, 124.0f, 245.0f } / 255.0f;
